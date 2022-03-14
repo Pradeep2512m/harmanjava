709eaa3b6293f71ac18ba9fb6c71b615e0900d27
pradeep2512m <pradeepm6383@gmail.com>
2022-03-14 12:26:37 +0530

    java

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        Integer a=10;
+
+        if(a<10)
+        {
+
+
+            System.out.println("its less than 10");
+        }
+        else {
+
+            System.out.println("its greater than 10");
+        }
+
+    }
+}
